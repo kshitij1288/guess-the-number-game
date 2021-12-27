42498ddc9e0faec3387447ebbb352f345d0826b8
kshitij1288 <74482019+kshitij1288@users.noreply.github.com>
2021-12-27 14:58:42 +0530

    Add files via upload

---
+++ guessTheNum.py
@@ -0,0 +1,31 @@
+# Dan's number guessing game
+
+# Import modules
+import random
+
+# Intro
+print("Welcome number guessing game!")
+
+# Define variables
+guesses = 0
+number = random.randint(0, 10)
+
+# Main program loop
+while( guesses < 3 ):
+    u_guess = int(input("Have a guess: "))
+
+    if u_guess == number:
+        print("Well done, you win!")
+        break
+    elif u_guess > number:
+        print("Your guess was too high.")
+        guesses = guesses + 1
+    elif u_guess < number:
+        print("Your guess was too low.")
+        guesses = guesses + 1
+    else:
+        print("There was an error.")
+
+if guesses == 5:
+    print("You lose!")
+    print("The number was: " + str(number))
